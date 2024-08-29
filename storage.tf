9903df7e23be94150b9b41ca466bf3d3c4efc7b1
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2024-08-29 09:04:00 +0000

    Updating Terraform Formatting

--- storage.tf
+++ storage.tf
@@ -13,12 +13,12 @@ locals {
 }
 
 module "storage-account" {
-  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                      = var.env
-  storage_account_name     = replace("${var.product}sa${var.env}", "-", "")
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
-  account_kind             = var.sa_account_kind
+  source               = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                  = var.env
+  storage_account_name = replace("${var.product}sa${var.env}", "-", "")
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = var.location
+  account_kind         = var.sa_account_kind
 
   account_tier             = var.sa_account_tier
   account_replication_type = var.sa_account_replication_type
